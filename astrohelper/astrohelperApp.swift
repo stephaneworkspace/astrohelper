693f01d226ede19cce6185b15d888ba5952ece18
Stéphane Bressani <stephane@bressani.dev>
2022-11-23 09:36:31 +0100

    Initial Commit

---
+++ astrohelper/astrohelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  astrohelperApp.swift
+//  astrohelper
+//
+//  Created by Stéphane on 23.11.22.
+//
+
+import SwiftUI
+
+@main
+struct astrohelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
